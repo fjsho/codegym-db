1ee5558f2b5e804d463605ba1ce8d8cc6622c5af
fjsho <gorimura7@gmail.com>
2021-06-19 19:08:46 +0900

    [add]データベース定義書及びsql文の作成

---
+++ db/db-challenge2/create_table.sql
@@ -0,0 +1,68 @@
+create table users(
+  id int(11) primary key auto_increment,
+  mail varchar(100) not null,
+  password varchar(100) not null,
+  user_name char(100) not null,
+  user_introduction varchar(1000),
+  office_phone char(13),
+  cell_phone char(13),
+  created_at datetime not null,
+  updated_at timestamp,
+  is_deleted tinyint(1) default 0 not null
+) engine InnoDB;
+
+create table chatrooms(
+  id int(11) primary key auto_increment,
+  dhat_name varchar(100) not null,
+  chat_description varchar(1000),
+  created_by_id int(11) not null,
+  created_at datetime not null,
+  updated_by_id int(11),
+  updated_at timestamp,
+  is_disabled_transmission tinyint(1) default 0 not null,
+  is_directchat tinyint(1) default 0 not null,
+  is_deleted tinyint(1) default 0 not null,
+  foreign key(created_by_id) references users(id),
+  foreign key(updated_by_id) references users(id)
+) engine InnoDB;
+
+create table chat_members(
+  user_id int(11) not null,
+  chat_id int(11) not null,
+  joined_at timestamp not null,
+  foreign key(user_id) references users(id),
+  foreign key(chat_id) references chatrooms(id)
+) engine InnoDB;
+
+create table posts(
+  id int(11) primary key auto_increment,
+  posted_by_id int(11) not null,
+  posted_description varchar(1000) not null,
+  posted_at datetime not null,
+  updated_by_id int(11),
+  updated_at timestamp,
+  posted_chat_id int(11) not null,
+  file_name varchar(100),
+  is_deleted tinyint(1) default 0 not null,
+  foreign key(posted_by_id) references users(id),
+  foreign key(updated_by_id) references users(id),
+  foreign key(posted_chat_id) references chatrooms(id)
+) engine InnoDB;
+
+create table tasks(
+  id int(11) primary key auto_increment,
+  task_description varchar(1000) not null,
+  person_in_charge_id int(11) not null,
+  due_at datetime,
+  created_by_id int(11) not null,
+  created_at datetime not null,
+  updated_by_id int(11),
+  updated_at timestamp,
+  registerd_chat_id int(11) not null,
+  is_completed tinyint(1) default 0 not null,
+  is_deleted tinyint(1) default 0 not null,
+  foreign key(person_in_charge_id) references users(id),
+  foreign key(created_by_id) references users(id),
+  foreign key(updated_by_id) references users(id),
+  foreign key(registerd_chat_id) references chatrooms(id)
+) engine InnoDB;
