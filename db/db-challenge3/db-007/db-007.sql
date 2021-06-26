7c7300caa815542d6a5deb66155d60e6e43a6cb7
fjsho <gorimura7@gmail.com>
2021-06-26 13:51:14 +0900

    Revert "[add]db-007.sqlの実装"
    This reverts commit cee0f5352f2e9baa5328e93cc3f0965e8a582e06.

--- db/db-challenge3/db-007/db-007.sql
+++
@@ -1,8 +0,0 @@
-select id, chat_name, chat_description, created_by_user_id, created_at, updated_by_user_id, updated_at,
-  case is_disabled_transmission when 0 then '許可'
-                                when 1 then '禁止'
-  end as is_disabled_transmission,
-is_directchat, is_deleted
-from chatrooms
-where is_deleted = 0 and chat_description like '%ダイレクトチャット'
-order by id;
