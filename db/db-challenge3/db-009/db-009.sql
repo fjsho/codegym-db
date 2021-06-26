b945de562196ae6261f98580268b2046cb178c9d
fjsho <gorimura7@gmail.com>
2021-06-26 13:50:22 +0900

    Revert "[add]db-009.sqlの実装"
    This reverts commit 7cb0f91860490fa4c33585f1c3b497e94bb3b3b7.

--- db/db-challenge3/db-009/db-009.sql
+++
@@ -1,9 +0,0 @@
-select count(*) as 投稿数, c.chat_name as チャットルーム名
-from posts as p
-right join chatrooms as c
-on p.posted_chat_id = c.id
-join users as u
-on p.posted_by_user_id = u.id
-where p.is_deleted = 0 and u.is_deleted = 0
-group by チャットルーム名
-order by 投稿数 desc
