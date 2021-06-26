176dd4da202afc10b6f53a3715800cc4bc11cf35
fjsho <gorimura7@gmail.com>
2021-06-26 13:51:07 +0900

    Revert "[add]db-008.sqlの実装"
    This reverts commit 592fedd9c3b20b4976f926f3f65e86816a49c785.

--- db/db-challenge3/db-008/db-008.sql
+++
@@ -1,8 +0,0 @@
-select u.user_name as ユーザー名, c.chat_name as チャット名, substring(joined_at,1,10) as 参加日時
-from chat_members
-join users as u
-on user_id = u.id
-join chatrooms as c
-on chat_id = c.id
-where u.is_deleted = 0 and c.is_deleted = 0
-order by 参加日時
