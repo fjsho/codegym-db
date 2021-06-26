a50572661bf9b9ff722e9fd26af106201ea82662
fjsho <gorimura7@gmail.com>
2021-06-26 16:08:32 +0900

    [fix]db-008：substringとソート対象カラムの修正

--- db/db-challenge3/db-008/db-008.sql
+++ db/db-challenge3/db-008/db-008.sql
@@ -1,8 +1,12 @@
-select u.user_name as ユーザー名, c.chat_name as チャット名, substring(joined_at,1,10) as 参加日時
-from chat_members
+select
+  u.user_name as ユーザー名,
+  c.chat_name as チャット名,
+  date_format(cm.joined_at,'%Y-%c-%d') as 参加日時,
+from chat_members as cm
 join users as u
-on user_id = u.id
+  on cm.user_id = u.id
 join chatrooms as c
-on chat_id = c.id
-where u.is_deleted = 0 and c.is_deleted = 0
-order by 参加日時
+  on cm.chat_id = c.id
+where u.is_deleted = 0
+  and c.is_deleted = 0
+order by cm.joined_at
