aac32bdddb1405233737bd042cac0619b160a938
fjsho <gorimura7@gmail.com>
2021-06-28 19:18:10 +0900

    [fix]可読性の改善(6,7,10)

--- db/db-challenge3/db-007/db-007.sql
+++ db/db-challenge3/db-007/db-007.sql
@@ -10,7 +10,8 @@ select
     when 0 then '許可'
     when 1 then '禁止'
   end as is_disabled_transmission,
-  is_directchat, is_deleted
+  is_directchat, 
+  is_deleted
 from chatrooms
 where is_deleted = 0
   and chat_description like '%ダイレクトチャット'
--- db/db-challenge3/db-010/db-010.sql
+++ db/db-challenge3/db-010/db-010.sql
@@ -11,10 +11,11 @@ where p.posted_at in (
   select max(p.posted_at)
   from posts as p
   join users as u
-  on p.posted_by_user_id = u.id
+    on p.posted_by_user_id = u.id
   join chatrooms as c
-  on p.posted_chat_id = c.id
-  where p.is_deleted = 0 and u.is_deleted = 0
+    on p.posted_chat_id = c.id
+  where p.is_deleted = 0
+    and u.is_deleted = 0
   group by p.posted_chat_id
   )
 order by c.id
