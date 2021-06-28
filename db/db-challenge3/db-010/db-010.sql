a79eebce6b30c0ef4b20862b87551a3ab5663795
fjsho <gorimura7@gmail.com>
2021-06-28 22:02:17 +0900

    [fix]db-010：where句の修正、スクショの更新

--- db/db-challenge3/db-010/db-010.sql
+++ db/db-challenge3/db-010/db-010.sql
@@ -8,14 +8,10 @@ join users as u
 join chatrooms as c
   on p.posted_chat_id = c.id
 where p.posted_at in (
-  select max(p.posted_at)
-  from posts as p
-  join users as u
-    on p.posted_by_user_id = u.id
-  join chatrooms as c
-    on p.posted_chat_id = c.id
-  where p.is_deleted = 0
-    and u.is_deleted = 0
-  group by p.posted_chat_id
+  select max(posted_at)
+  from posts
+  group by posted_chat_id
   )
+  and p.is_deleted = 0
+  and u.is_deleted = 0
 order by c.id
